54df83280381d04a876f03f9bc7711cae840baf9
Jörg Stenger <joerg.stenger@exozet.com>
2019-10-16 08:03:13 +0200

    Added tests for new, save, load endpoints

--- test/controllers/MainControllerSpec.scala
+++ test/controllers/MainControllerSpec.scala
@@ -27,6 +27,15 @@ class MainControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       contentAsString(home) must include ("----------------")
     }
 
+    "render the chess page and create a new board from a new instance of controller" in {
+      val controller = new MainController(stubControllerComponents())
+      val home = controller.chess().apply(FakeRequest(GET, "/chess/new"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) must include ("----------------")
+    }
+
     "render the chess page and move a piece from a new instance of controller" in {
       val controller = new MainController(stubControllerComponents())
       val home = controller.chess().apply(FakeRequest(GET, "/chess/move?fromRow=1&fromCol=1&toRow=2&toCol=1"))
@@ -36,22 +45,22 @@ class MainControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       contentAsString(home) must include ("----------------")
     }
 
-//    "render the index page from the application" in {
-//      val controller = inject[MainController]
-//      val home = controller.index().apply(FakeRequest(GET, "/"))
-//
-//      status(home) mustBe OK
-//      contentType(home) mustBe Some("text/html")
-//      contentAsString(home) must include ("Welcome to Play")
-//    }
-//
-//    "render the index page from the router" in {
-//      val request = FakeRequest(GET, "/")
-//      val home = route(app, request).get
-//
-//      status(home) mustBe OK
-//      contentType(home) mustBe Some("text/html")
-//      contentAsString(home) must include ("Welcome to Play")
-//    }
+    "render the chess page and ssave from a new instance of controller" in {
+      val controller = new MainController(stubControllerComponents())
+      val home = controller.chess().apply(FakeRequest(GET, "/chess/save"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) must include ("----------------")
+    }
+
+    "render the chess page and load from a new instance of controller" in {
+      val controller = new MainController(stubControllerComponents())
+      val home = controller.chess().apply(FakeRequest(GET, "/chess/load"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) must include ("----------------")
+    }
   }
 }
